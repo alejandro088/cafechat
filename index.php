5d3306daa6f7d498d2f797b972840fdf6c59664a
Alejandro <snackbarchat@gmail.com>
2020-07-07 14:36:30 -0300

    add kiwi irc

--- index.php
+++ index.php
@@ -46,10 +46,12 @@
             	<h4> Te esperamos en #CafeChat para compartir buenos momentos.</h4>
 
             	<div class="links m-3">
-            		
-            		    <a class="m-2 btn btn-primary btn-block" href="http://irc.cafechat.com.ar/#Cafe">Chatea en #Cafe!</a>
 
-           		    <a class="m-2 btn btn-primary btn-block" href="http://irc.cafechat.com.ar/#CafeGitano">Chatea en #CafeGitano!</a>
+						<input type="text" id="nick" name="nick" class="form-control m-2" placeholder="Ingresá tu nick..." />
+            		
+            		    <a id="cafe" class="m-2 btn btn-primary btn-block" href="http://irc.cafechat.com.ar/#Cafe">Chatea en #Cafe!</a>
+
+           		    <a id="cafegitano" class="m-2 btn btn-primary btn-block" href="http://irc.cafechat.com.ar/#CafeGitano">Chatea en #CafeGitano!</a>
            
             	</div>
 
@@ -61,5 +63,22 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+    <script>
+
+    	$(document).ready(function() {
+
+    	$("#nick").change(function(){
+
+    	let nick = $("#nick").val()
+
+    	$("#cafe").attr("href", `http://irc.cafechat.com.ar/?nick=${nick}#Cafe`)
+
+    	$("#cafegitano").attr("href", `http://irc.cafechat.com.ar/?nick=${nick}#CafeGitano`)
+
+        });
+
+    	});
+    </script>
 </body>
 </html>
